1b7acf05cd9639f45b0e30686a55fb680c9106dc
lehcode <53556648+lehcode@users.noreply.github.com>
2024-12-15 18:43:45 +0200

    feat: Add settings form

---
+++ src/app/components/forms/settings-form.schema.ts
@@ -0,0 +1,30 @@
+import { Validators } from "@angular/forms";
+
+import { IFormField } from "../../interfaces/form-field.interface";
+
+export const settingsFormSchema: IFormField[] = [
+    {
+      "name": "fallingSpeed",
+      "label": "Falling Speed",
+      "error": "Falling speed is required",
+      validators: [Validators.required, Validators.min(10), Validators.max(50)]
+    },
+    {
+      name: "fallingFrequency",
+      label: 'Falling Frequency',
+      error: 'Frequency is required',
+      validators: [Validators.required, Validators.min(10), Validators.max(50)]
+    },
+    {
+      name: "playerSpeed",
+      label: 'Player Speed',
+      error: 'Player Speed is required',
+      validators: [Validators.required, Validators.min(10), Validators.max(50)]
+    },
+    {
+      name: "gameTime",
+      label: 'Game Time',
+      error: 'Game Time is required',
+      validators: [Validators.required, Validators.min(10), Validators.max(50)]
+    }
+  ]
